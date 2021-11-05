1b8461ec8156057a717c346cc1add2ac2d2ea4e1
Easwaran <varunwho>
2021-11-05 12:28:40 +0530

    final

--- src/App.js
+++ src/App.js
@@ -6,20 +6,20 @@ const DUMMY_EXPENSES = [
   {
     id: 'e1',
     title: 'Toilet Paper',
-    amount: 94.12,
+    amount: 100,
     date: new Date(2020, 7, 14),
   },
-  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+  { id: 'e2', title: 'New TV', amount: 85000, date: new Date(2021, 2, 12) },
   {
     id: 'e3',
     title: 'Car Insurance',
-    amount: 294.67,
+    amount: 25000,
     date: new Date(2021, 2, 28),
   },
   {
     id: 'e4',
     title: 'New Desk (Wooden)',
-    amount: 450,
+    amount: 10000,
     date: new Date(2021, 5, 12),
   },
 ];
